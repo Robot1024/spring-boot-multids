2f4fd99992a304696b8966e864fe9dd4c0ce6664
penglei <943782585@qq.com>
2017-02-10 15:24:20 +0800

    spring multids  配置

---
+++ src/main/java/com/multids/db/App.java
@@ -0,0 +1,22 @@
+package com.multids.db;
+
+/**
+ * @crea : Created by intelliJ IDEA 16.1.1 .
+ * @auth : PengLei 18910335440@189.com .
+ * @date : 2017/2/10
+ * @desc :
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Angel(QQ:412887952)
+ * @version v.0.1
+ */
+@SpringBootApplication
+public class App {
+        public static void main(String[] args) {
+            SpringApplication.run(App.class, args);
+        }
+}
